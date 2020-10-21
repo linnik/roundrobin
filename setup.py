4993ded1c4c2e1c4cd948ab6e8ca11055fddf06a
Vyacheslav Linnik <vyacheslav.linnik@gmail.com>
2020-10-21 12:42:40 +0300

    extend package classifiers

--- setup.py
+++ setup.py
@@ -20,8 +20,16 @@ def setup():
         license="MIT",
         classifiers=[
             "License :: OSI Approved :: MIT License",
+            'Programming Language :: Python',
             "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2.7",
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Operating System :: OS Independent',
         ],
         packages=setuptools.find_packages(),
     )
